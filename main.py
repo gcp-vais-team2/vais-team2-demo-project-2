216eb5d4087527489e347bc713a33506416a700a
avinashjay1 <Avinash.Veershetty@bristlecone.com>
2025-07-22 18:26:33 +0530

    Update content of main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+print('Welcome to Demo Project 2')
+
 def main():
-    print('App started with ENV: second')
+    print('This is the main logic for project 2')
 
 if __name__ == '__main__':
     main()
